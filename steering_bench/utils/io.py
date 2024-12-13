d3102f2a5e6d922028087f7271476c6bea3021b8
Daniel Tan <dtch1997@users.noreply.github.com>
2024-12-13 23:53:23 +0000

    download MWE v2

--- steering_bench/utils/io.py
+++ steering_bench/utils/io.py
@@ -7,7 +7,7 @@ import io
 import json
 import os
 
-from typing import Any
+from typing import Any, Sequence
 
 
 def _make_w_io_base(f, mode: str):
@@ -32,6 +32,22 @@ class DataclassEncoder(json.JSONEncoder):
         if is_dataclass(o):
             return asdict(o)  # type: ignore
         return json.JSONEncoder.default(self, o)
+
+
+def read_jsonl(f, mode="r") -> Any:
+    """Load a .jsonl file into a list."""
+    f = _make_r_io_base(f, mode)
+    jlist = [json.loads(line) for line in f]
+    f.close()
+    return jlist
+
+
+def write_jsonl(obj: Sequence[Any], f, mode="w"):
+    """Write a list to a .jsonl file."""
+    f = _make_w_io_base(f, mode)
+    for line in obj:
+        f.write(json.dumps(line, ensure_ascii=False) + "\n")
+    f.close()
 
 
 def jdump(obj, f, mode="w", indent=4):
